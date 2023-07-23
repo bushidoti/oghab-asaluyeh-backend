cdc805b3413bf10657605855de2686d83b3c039a
sajjad <titanxl79@gmail.com>
2023-07-23 10:22:33 +0330

    dynamic serializer property and personal management

--- documentManagement/serializer.py
+++ documentManagement/serializer.py
@@ -2,7 +2,22 @@ from rest_framework import serializers
 from .models import Document
 
 
-class DocumentSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        fields = self.context['request'].query_params.get('fields')
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class DocumentSerializer(DynamicFieldsModelSerializer, serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = '__all__'
--- propertyManagement/serializer.py
+++ propertyManagement/serializer.py
@@ -2,13 +2,28 @@ from rest_framework import serializers
 from .models import Property, Person
 
 
-class PropertySerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        fields = self.context['request'].query_params.get('fields')
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class PropertySerializer(DynamicFieldsModelSerializer, serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = '__all__'
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonSerializer(DynamicFieldsModelSerializer, serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
