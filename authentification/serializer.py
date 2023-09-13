12ed5296c5545bee66b68436975815c54cd72602
sajjad <titanxl79@gmail.com>
2023-09-13 21:06:01 +0330

    authentication

---
+++ authentification/serializer.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from authentification.models import Employee, Maintenance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class MaintenanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Maintenance
+        fields = '__all__'
