85a4789e3e779e6b4b8c587daa7cedf2635804c3
Sajjad <titanxl79@gmail.com>
2023-10-19 08:57:46 +0330

    customized admin page

--- propertyManagement/admin.py
+++ propertyManagement/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Person, Property
 
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'full_name', 'caseNumber', 'sex',
+                    'date', 'national_id', 'job', 'approvedPrice', 'commitmentPrice', 'typeBail', 'firstBail',
+                    'secondBail', 'expireDate',
+                    'clearedStatus', 'clearedDate', 'receivedDocument', 'affidavitStatus', 'office']
+    list_per_page = 20
+    list_filter = ['office', 'type', 'sex']
+    search_fields = (
+        "national_id",
+        "id",
+        "caseNumber",
+    )
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Property)
