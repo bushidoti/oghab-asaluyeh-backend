7854935e53470bb6c3bcc37f295217f5fe7a1818
Sajjad <titanxl79@gmail.com>
2023-11-10 18:02:07 +0330

    add admin to document. change label names

--- documentManagement/admin.py
+++ documentManagement/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Document
 
-admin.site.register(Document)
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type_form', 'name', 'contractNumber', 'topicContract',
+                    'dateContract', 'typeContract', 'durationContract', 'contractPrice', 'prePaidPrice', 'goodPrice',
+                    'typeBail1', 'firstBail', 'secondBail', 'commitmentPrice', 'typeBail2', 'firstBail2',
+                    'secondBail2', 'clearedStatus', 'clearedDate', 'receivedDocument', 'office']
+    list_per_page = 20
+    list_filter = ['type_form', 'typeContract']
+    search_fields = (
+        "id",
+        "name",
+        "contractNumber",
+        "topicContract",
+        "typeContract",
+    )
+
+
+admin.site.register(Document, DocumentAdmin)
